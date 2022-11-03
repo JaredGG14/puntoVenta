8947f85899f60d30136e0a0aa0ce75cc1d06050f
davox33 <116908232+davox33@users.noreply.github.com>
2022-11-03 09:28:02 -0600

    Renaming files

--- app/Models/Provider.php
+++ app/Models/Provider.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Product;
+use App\Models\Purchase;
+use App\Models\DeliveryDay;
+use App\Models\Enterprise;
 
 class Provider extends Model
 {
@@ -17,6 +20,18 @@ class Provider extends Model
     ];
 
     public function products(){
-        return $this->hasMany(Product::class);
+        return $this->hasMany(Product::class,'provider_id','id');
+    }
+
+    public function purchases(){
+        return $this->hasMany(Purchase::class,'provider_id','id');
+    }
+
+    public function delivery_days(){
+        return $this->hasMany(DeliveryDay::class,'provider_id','id');
+    }
+
+    public function enterprises(){
+        return $this->belongsTo(Enterprise::class,'enterprise_id');
     }
 }
