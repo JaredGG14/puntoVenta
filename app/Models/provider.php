5a2ef60c61a7343fbc4963d6a046dc1b3715297a
JaredGG14 <19151687@aguascalientes.tecnm.mx>
2022-11-05 15:58:09 -0600

    Axios, Bootstrap/React-Bootstrap, react-router-dom Installation

--- app/Models/provider.php
+++ app/Models/provider.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Product;
 
-class provider extends Model
+class Provider extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'name',
+        'last_name',
+        'cellphone',
+        'enterprise_id'
+    ];
+
+    public function products(){
+        return $this->hasMany(Product::class);
+    }
 }
