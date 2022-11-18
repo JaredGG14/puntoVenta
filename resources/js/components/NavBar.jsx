3aef2ccf73f8724125c61dcf1f110aba55c9a2ae
AdharaShiro <andybrides06@gmail.com>
2022-11-18 15:18:35 -0600

    Starting template 1.3

--- resources/js/components/NavBar.jsx
+++ resources/js/components/NavBar.jsx
@@ -1,3 +1,4 @@
+import { Image } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -6,10 +7,16 @@ import { Outlet, Link } from 'react-router-dom';
 function NavBar() {
   return (
     <>
-      <Navbar bg="dark" variant="dark">
+      <Navbar bg="dark" variant=''>
         <Container>
-          <Navbar.Brand as = {Link} to="/puntoVenta/public/" >
-          
+          <Navbar.Brand to="/puntoVenta/public/" >
+          <img
+              src="resources\images\LogoPV.png"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+              alt="Logo"
+          />
           </Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link as = {Link} to="/puntoVenta/public/products">Productos</Nav.Link>
