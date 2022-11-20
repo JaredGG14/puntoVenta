9d585cfd0d8b4c2c56adde11d1854c729c19e1c2
JaredGG14 <19151687@aguascalientes.tecnm.mx>
2022-11-20 17:32:28 -0600

    Bugs bugs bugs

--- resources/js/components/Product/Product.jsx
+++ resources/js/components/Product/Product.jsx
@@ -92,17 +92,10 @@ const ShowProducts = () => {
                         <th>Provider</th>
                         <th>Categories</th>
                         <th>
-<<<<<<< HEAD
-                        <div className="input-group mb-3">
-                                <div className="input-group-text">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-                                  <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
-=======
                             <div className="input-group input-group-sm">
                                 <div className="input-group-text">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
                                         <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
->>>>>>> 3176aecd1c3f692f6014a8c5990d821a72d2b123
                                     </svg>
                                 </div>
                                 <input type="text" size className="form-control form-control-sm" placeholder="Search Product..." aria-label="Search Product" aria-describedby="button-addon2" />
